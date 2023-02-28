7020ff09c9924c6288c7c665303fcf1affc676a5
Este91 <esteban.a.boffa@gmail.com>
2023-02-28 16:39:46 -0300

    Initial commit

---
+++ trustWalletChallenge/trustWalletChallenge/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  trustWalletChallenge
+//
+//  Created by Esteban Boffa on 28/02/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print("hello")
+    }
+
+
+}
+
