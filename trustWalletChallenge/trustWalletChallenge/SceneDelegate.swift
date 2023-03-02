5940c19da86920ada660ad437e1b550dfe088c86
Este91 <esteban.a.boffa@gmail.com>
2023-03-01 22:54:33 -0300

    Set rootViewController in SceneDelegate

--- trustWalletChallenge/trustWalletChallenge/SceneDelegate.swift
+++ trustWalletChallenge/trustWalletChallenge/SceneDelegate.swift
@@ -9,14 +9,22 @@ import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
-    var window: UIWindow?
+    // MARK: Properties
 
+    private var appCoordinator: AppCoordinator?
+    var window: UIWindow?
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        appCoordinator = AppCoordinator(presentationStyle: .pushed(UINavigationController()))
+        guard let viewController = appCoordinator?.start() else { return }
+        let window = UIWindow(windowScene: windowScene)
+        window.rootViewController = viewController
+        self.window = window
+        window.makeKeyAndVisible()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
